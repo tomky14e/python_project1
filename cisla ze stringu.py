28185a2348947da9cad72f3c1a2dd3f285880e8a
tomky14e <tomas.matejicek@hotmail.com>
2024-01-08 20:54:27 +0100

    deleted:    "D\303\251lka slov, comprehensions.py" 	deleted:    cisla ze stringu.py 	deleted:    delitel.py 	deleted:    delitel.txt 	deleted:    fizz buzz.py 	deleted:    sachovnice.py 	deleted:    script na serazeni.py 	deleted:    souhlasky a samohlasky.py 	deleted:    suda a licha cisla.py 	deleted:    task_template.py 	deleted:    test update slovniku.py

--- cisla ze stringu.py
+++
@@ -1,8 +0,0 @@
-vysledek = list()
-zadana_cisla = ('1, 2, 3, 4, 5')
-cisla = zadana_cisla.split(",")
-
-for cislo in cisla:
-    vysledek.append(int(cislo.strip()))
-
-print(f'List: {vysledek}')
--- fizz buzz.py
+++
@@ -1,9 +0,0 @@
-for cislo in range(1, 101):
-    if (cislo % 3 == 0) and (cislo % 5 == 0):
-        print('FizzBuzz')
-    elif cislo % 3 == 0:
-        print('Fizz')
-    elif cislo % 5 == 0:
-        print('Buzz')
-    else:
-        print(cislo)
